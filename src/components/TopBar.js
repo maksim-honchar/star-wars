a2bbc9a24ad6ee15384a70d9a6e3d1a7778f42f6
Maksym Honchar <maksim.honchar@gmail.com>
2020-11-04 14:19:06 +0200

    Revert "update option  - back to home page"
    This reverts commit 3754bf403bd299a6281e272843963212757b0c83.

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { updateCurrentPage } from '../redux/planetsSlice'
-
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { Link } from 'react-router-dom'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -19,32 +16,20 @@ const useStyles = makeStyles((theme) => ({
     },
     link: {
         color: '#ffeb3b',
-        textDecoration: 'none',
-        cursor: 'pointer'
+        textDecoration: 'none'
     }
 }))
 
 
 export const TopBar = () => {
     const classes = useStyles();
-    const dispatch = useDispatch()
-    const history = useHistory()
-
-    const onToHome = () => {
-        dispatch(updateCurrentPage(0))
-        history.push('/')
-    }
-
 
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6" className={classes.title}>
-                        {/* <Link to="/" className={classes.link}>Star Wars</Link> */}
-                        <span className={classes.link} onClick={onToHome}>
-                            Star Wars
-                        </span>
+                        <Link to="/" className={classes.link}>Star Wars</Link>
                     </Typography>
                 </Toolbar>
             </AppBar>
