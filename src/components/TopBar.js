3754bf403bd299a6281e272843963212757b0c83
Maksym Honchar <maksim.honchar@gmail.com>
2020-11-04 13:45:23 +0200

    update option  - back to home page

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { updateCurrentPage } from '../redux/planetsSlice'
+
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import { Link } from 'react-router-dom'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -16,20 +19,32 @@ const useStyles = makeStyles((theme) => ({
     },
     link: {
         color: '#ffeb3b',
-        textDecoration: 'none'
+        textDecoration: 'none',
+        cursor: 'pointer'
     }
 }))
 
 
 export const TopBar = () => {
     const classes = useStyles();
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    const onToHome = () => {
+        dispatch(updateCurrentPage(0))
+        history.push('/')
+    }
+
 
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6" className={classes.title}>
-                        <Link to="/" className={classes.link}>Star Wars</Link>
+                        {/* <Link to="/" className={classes.link}>Star Wars</Link> */}
+                        <span className={classes.link} onClick={onToHome}>
+                            Star Wars
+                        </span>
                     </Typography>
                 </Toolbar>
             </AppBar>
